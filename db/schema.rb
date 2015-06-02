bb1b3d4962257a52f35ab258f1a82c5578da6b3f
Stewart DeSoto <stewartdesoto@yahoo.com>
2015-06-02 00:21:58 +0000

    add admin user with ability to delete recipes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528145903) do
+ActiveRecord::Schema.define(version: 20150601220159) do
 
   create_table "chefs", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150528145903) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
   create_table "ingredients", force: true do |t|
